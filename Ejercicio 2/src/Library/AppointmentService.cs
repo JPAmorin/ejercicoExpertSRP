f36829489413d635c7215bdc5156be491541a760
JPAmorin <juanpabloamorinjusto@gmail.com>
2022-09-04 20:55:48 -0300

    No se.

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -5,11 +5,26 @@ namespace Library
 {
     public class AppointmentService
     {
-        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        public string Name { get; }
+        public string Id { get; }
+        public string PhoneNumber { get; }
+        public DateTime Date { get; }
+        public string AppoinmentPlace { get; }
+        public string DoctorName { get; }
+        public AppointmentService(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
         {
-            StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
+            this.Name = name;
+            this.Id = id;
+            this.PhoneNumber = phoneNumber;
+            this.Date = date;
+            this.AppoinmentPlace = appoinmentPlace;
+            this.DoctorName = doctorName;
+            StringBuilder stringBuilder = new StringBuilder("");
             Boolean isValid = true;
 
+        }
+        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+        {
             if (string.IsNullOrEmpty(name))
             {
                 stringBuilder.Append("Unable to schedule appointment, 'name' is required\n");
@@ -48,6 +63,15 @@ namespace Library
 
             return stringBuilder.ToString();
         }
-
+        public static string NameValidator(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                stringBuilder.Append("Unable to schedule appointment, 'name' is required\n");
+                isValid = false;
+            }
+            return stringBuilder.ToString();
+        }
     }
 }
+//No se.
